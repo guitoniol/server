1608812561204bd222d85dbbb90108ddf7fb5321
Ymiii <gui.toniol@gmail.com>
2020-06-26 14:32:11 -0300

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   src/auth.json 	new file:   src/config/uploadConfig.ts 	new file:   src/controllers/productsController.ts 	new file:   src/controllers/sellesrController.ts 	new file:   src/models/Product.ts 	new file:   src/models/Seller.ts 	new file:   src/routes.ts 	new file:   src/server.ts 	new file:   tsconfig.json

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express'; 
+import mongoose from 'mongoose'
+import cors from 'cors';
+
+import routes from './routes';
+
+const bd = process.env.bd || require('./auth.json').bd;
+const app = express();
+console.log(bd);
+
+mongoose.connect(`mongodb+srv://${bd}@cluster0-8coeh.mongodb.net/ecomilhas?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+console.log("'-'")
+app.listen(3333);
+
